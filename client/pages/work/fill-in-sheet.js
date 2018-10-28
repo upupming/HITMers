4b34a440a0e4a52fcacdbd6ec262cb7f101f419a
upupming <upupming@gmail.com>
2018-10-28 16:37:30 +0800

    Improved: sheet styles

--- client/pages/work/fill-in-sheet.js
+++ client/pages/work/fill-in-sheet.js
@@ -290,7 +290,7 @@ Page({
     let headHTML = `<head>
       <style>
         html {
-          font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+          font-family: Baskerville, Georgia, "Liberation Serif", "Kaiti SC", STKaiti, "AR PL UKai CN", "AR PL UKai HK", "AR PL UKai TW", "AR PL UKai TW MBE", "AR PL KaitiM GB", KaiTi, KaiTi_GB2312, DFKai-SB, "TW\-Kai", serif;
           background-color: #99d3f9;
         }
 
@@ -302,6 +302,7 @@ Page({
           background: linear-gradient(to right, #5d8ef1, #25b7c4);
           margin-top: 3em;
           width: 90%;
+          color: white;
         }
 
         td, th {
@@ -328,6 +329,10 @@ Page({
         .studying {
           background: #99d3f9;
         }
+
+        .day-date+tr {
+          font-weight: bold;
+        }
       </style>
     </head>`;
 
@@ -347,8 +352,8 @@ Page({
     for (let dayIndex = 0; dayIndex < this.data.shifts.length; dayIndex++) {
       // For empty day
       if(this.data.shifts[dayIndex][0].length === 0 && this.data.shifts[dayIndex][1].length === 0) {
-        sheetHTML += `<tr class="day">
-          <th class="day-date">${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
+        sheetHTML += `<tr class="day day-date">
+          <th>${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
           <td class="morning">&nbsp;</td>
           <td class="afternoon">&nbsp;</td>
         </tr>`;
@@ -359,8 +364,8 @@ Page({
         let dayRowspan = this.data.shifts[dayIndex][1].length;
         let commonSpan = this.data.shifts[dayIndex][0].length;
 
-        sheetHTML += `<tr class="day">
-          <th class="day-date" rowspan="${dayRowspan+1}">${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
+        sheetHTML += `<tr class="day day-date">
+          <th rowspan="${dayRowspan+1}">${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
         </tr>`;
         // Morning shifts and equal size afternoon shifts
         for(let i=0; i<commonSpan; i++) {
@@ -381,8 +386,8 @@ Page({
         let dayRowspan = this.data.shifts[dayIndex][0].length;
         let commonSpan = this.data.shifts[dayIndex][1].length;
 
-        sheetHTML += `<tr class="day">
-          <th class="day-date" rowspan="${dayRowspan+1}">${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
+        sheetHTML += `<tr class="day day-date">
+          <th rowspan="${dayRowspan+1}">${this.data.language.months[this.data.monthIndices[dayIndex]] + ' ' + this.data.language.days[this.data.dayIndices[dayIndex]] + ' ' + this.data.language.weekDays[dayIndex]}</th>
         </tr>`;
         // Morning shifts and equal size afternoon shifts
         for(let i=0; i<commonSpan; i++) {
