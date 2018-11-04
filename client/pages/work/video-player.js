aa62d2aa39ff9d1d74be55764559ac55111b355d
upupming <upupming@gmail.com>
2018-11-05 00:36:44 +0800

    Improved: not proxy streamable for faster speed

--- client/pages/work/video-player.js
+++ client/pages/work/video-player.js
@@ -1,6 +1,5 @@
 const request = require('../../utils/requests');
 const video = require('../../videos');
-const config = require('../../config');
 
 Page({
   data: {
@@ -12,7 +11,6 @@ Page({
     request.getRawVideoUrl(params.video_code, video.username, video.password)
       .then(res => {
         let url = res.data.files['mp4'].url;
-        url = url.replace(config.service.streamCDN, config.service.streamCDNProxy);
         this.setData({
           videoUrl: 'https:' + url,
           loading: false
